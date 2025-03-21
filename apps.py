9012e311fd834abebf1d43e68b691f9669ff3926
tougshire <tougshire@tougshire.com>
2025-03-21 10:30:57 -0400

    Fix name of WibekwaBaseConfig

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SdcvirginiaBaseConfig(AppConfig):
+class WibekwaBaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wibekwa_base'
